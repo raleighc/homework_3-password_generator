a51a8b88ecbdf4b3b8fe856e8da364b8677fab2b
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-09-03 18:17:15 -0400

    Finally got the click button to operate. have a starting point to begin inputting prompt and confirms

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
@@ -8,8 +6,8 @@ var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".",
-":", ";" , "=", , "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
-
+":", ";" , "=", , "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
@@ -21,9 +19,17 @@ function writePassword() {
 }
 
 function generatePassword() {
-  // TODO: generate a real password and return password string
-  return "THIS IS NOT MY FINAL PASSWORD!";
+  var password = "";
+  var length = prompt("How many characters would you like in your password? Enter and number from 8-128");
+    if (length < 7 || length > 129) {
+      alert("Your password can only contain 8-128 characters.");
+      return "good job, you selected your character length!";
+      
+    }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
