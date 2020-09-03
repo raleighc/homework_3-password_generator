bf2b84d82a103b41ce51cb7699af5003b9195217
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-09-03 19:57:44 -0400

    created all my confirms and attempted to push them into the password string. seeing all prompts and confirms but not getting any results yet.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-
+var generateBtn = document.querySelector("#generate");
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -7,7 +7,7 @@ var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".",
 ":", ";" , "=", , "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
-var generateBtn = document.querySelector("#generate");
+
 
 
 // Write password to the #password input
@@ -20,12 +20,37 @@ function writePassword() {
 
 function generatePassword() {
   var password = "";
-  var length = prompt("How many characters would you like in your password? Enter and number from 8-128");
-    if (length < 7 || length > 129) {
+  var pLength = prompt("How many characters would you like in your password? Enter and number from 8-128");
+    if (pLength < 8 || pLength > 128) {
       alert("Your password can only contain 8-128 characters.");
-      return "good job, you selected your character length!";
-      
+      return "Click to try again.";
+    } else {
+      var confirmUpCase = confirm("If you would like to include uppercase letters, click ok.");
+      if (confirmUpCase == true) {
+        password += upCase;
+      }
+      var confirmLowCase = confirm("If you would like to include lowercase letters, click ok.");
+      if (confirmLowCase == true) {
+        password += lowCase;
+      }
+      var confirmNumbers = confirm("If you would like to include numbers, click ok.");
+      if (confirmNumbers == true) {
+        password += numbers;
+      }
+      var confirmSpecChar = confirm("If you would like to include special characters, click ok.");
+      if (confirmSpecChar == true) {
+        password += specChar;
+      }
+      if (confirmUpCase == false && confirmLowCase == false && confirmNumbers == false && confirmSpecChar == false){
+        alert("You must select at least one character type.");
+        return "Click to try again.";
+      }
+      for (var i = 0; i < pLength; i++) {
+        var randomCharacter = Math.floor(Math.random() * password.length);
+        result = result + password[randomCharacter];
+      }
     }
+    
 }
 
 
