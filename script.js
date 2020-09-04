5c277f5695df9dd61bc7a781611252a7b94f8680
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-09-03 22:14:14 -0400

    changed all arrays to strings because commas for separating index items were populating my password. added comments to claify what each section of my code is accomplishing.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".",
-":", ";" , "=", , "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+// List of variables that will be called on by confirms
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789";
+var specChar = "!#$%&'()*+,-.:;=?@[]^_{|}~";
 
+// Empty variables that will be populated by if/else confirms
+var tempResult = "";
 var endResult = "";
 
 
@@ -20,44 +20,57 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Clicking generate button begins this function
 function generatePassword() {
-  var tempResult = "";
   var pLength = prompt("How many characters would you like in your password? Enter and number from 8-128");
-    if (pLength < 8 || pLength > 128) {
+    // if statement limits user to only inputting numbers from 8-128 including characters that aren't numbers
+    if (isNaN(pLength) || pLength < 8 || pLength > 128) {
       alert("Your password can only contain 8-128 characters.");
-      return "Click to try again.";
+      return "Click button to try again.";
+      // once if criteria is met, this sequence of confirm questions will kick off from asking the user which character types they would like to include in their password.
     } else {
-      var confirmUpCase = confirm("If you would like to include uppercase letters, click ok.");
+      var confirmUpCase = confirm("If you would like to include uppercase letters, click ok. If not, click cancel");
       if (confirmUpCase == true) {
         tempResult += upCase;
       }
-      var confirmLowCase = confirm("If you would like to include lowercase letters, click ok.");
+      var confirmLowCase = confirm("If you would like to include lowercase letters, click ok. If not, click cancel");
       if (confirmLowCase == true) {
         tempResult += lowCase;
       }
-      var confirmNumbers = confirm("If you would like to include numbers, click ok.");
+      var confirmNumbers = confirm("If you would like to include numbers, click ok. If not, click cancel");
       if (confirmNumbers == true) {
         tempResult += numbers;
       }
-      var confirmSpecChar = confirm("If you would like to include special characters, click ok.");
+      var confirmSpecChar = confirm("If you would like to include special characters, click ok. If not, click cancel");
       if (confirmSpecChar == true) {
         tempResult += specChar;
       }
+
+      // If the user does not select any character types this will notify them and return to the beginning of the process.      
       if (confirmUpCase == false && confirmLowCase == false && confirmNumbers == false && confirmSpecChar == false){
         alert("You must select at least one character type.");
-        return "Click to try again.";
+        return "Click button to try again.";
       }
+
+      // after the length and confirms have been successfully completed, this for loop with randomize the characters selected and combine them into the final password.
       for (var i = 0; i < pLength; i++) {
+        // console.log(pLength);
         var tempCharacters = Math.floor(Math.random() * tempResult.length);
+        // console.log(tempCharacters);
         var randomized = tempResult[tempCharacters];
+        // console.log(randomized);
         endResult = endResult + randomized;
+        // console.log(endResult);
+        
       }
     }
+    // final return prints the generated password into the textarea.
     return endResult;
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
 
